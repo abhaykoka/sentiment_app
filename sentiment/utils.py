0d17cd8f726bf812da9c7e39bfa09a0097fd1a6e
abhaykoka <135577877+abhaykoka@users.noreply.github.com>
2024-12-17 15:50:00 +0530

    Update utils.py

--- sentiment/utils.py
+++ sentiment/utils.py
@@ -3,6 +3,12 @@ import pickle
 from django.conf import settings
 import requests
 import joblib
+url="https://github.com/abhaykoka/sentiment_app/blob/main/ml_Models/sentiment_model.pkl"
+response = requests.get(url)
+
+with open("model_file.pkl", "wb") as f:
+    f.write(response.content)
+model = joblib.load("model_file.pkl")
 MODEL_PATH = os.path.join("ml_Models\\sentiment_model.pkl")
 
 with open(MODEL_PATH, 'rb') as f:
